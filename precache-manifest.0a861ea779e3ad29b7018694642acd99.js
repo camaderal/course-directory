59355d938f018b2411b8055c934948de79d9c9f4
Christeena <camaderal@gmail.com>
2022-07-10 20:02:07 +0900

    Updates

---
+++ precache-manifest.0a861ea779e3ad29b7018694642acd99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35fa44e1de24d304786d",
+    "url": "/react-app-sample/static/css/main.112faac4.chunk.css"
+  },
+  {
+    "revision": "35fa44e1de24d304786d",
+    "url": "/react-app-sample/static/js/main.35fa44e1.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/react-app-sample/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "24d9d7c8801a485e5a15",
+    "url": "/react-app-sample/static/js/runtime~main.24d9d7c8.js"
+  },
+  {
+    "revision": "d6abf1000a4cd0fe59571a31f6a009a5",
+    "url": "/react-app-sample/index.html"
+  }
+];
